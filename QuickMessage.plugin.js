fab840fc69e5c6651537a4125832271e2b15008d
Christian Grothaus <cgrothaus@outlook.com>
2024-07-26 18:13:07 -0400

    init

---
+++ QuickMessage.plugin.js
@@ -0,0 +1,20 @@
+/**
+ * @name QuickMessage
+ * @author Capy
+ * @description A plugin for adding back the option for sending messages in the user popout.
+ * @version 0.0.1
+ */
+
+module.exports = class MyPlugin {
+  constructor(meta) {
+    // Do stuff in here before starting
+  }
+
+  start() {
+    // Do stuff when enabled
+  }
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
